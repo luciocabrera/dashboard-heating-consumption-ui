087d2e3a03f32df16a17cecf7d44df4c611121e0
Lucio Cabrera <luciocabrera1978@gmail.com>
2020-07-06 19:29:23 +0000

    Set up basic form for the logs

--- src/Layouts/log/LogForm.js
+++ src/Layouts/log/LogForm.js
@@ -1,53 +1,102 @@
-import React, { useState } from './node_modules/react';
-import { Form, Button, Radio, Select, DatePicker, InputNumber } from './node_modules/antd';
+// React
+import React, { useState, useEffect } from 'react';
+// Router
+import { useHistory } from 'react-router-dom';
+// Components
+import { FormCreate } from '../../components/index';
+import { notification } from 'antd';
+// Services
+import * as logService from '../../services/logService';
+
+const formItems = [
+  {
+    name: 'date',
+    label: 'Date',
+    type: 'datePicker',
+    rules: [
+      {
+        required: true,
+        message: 'Please enter the Code!',
+      },
+      { min: 3, message: 'The Code must be of at least 3 characters' },
+      { max: 24, message: 'The Code must be of maximum 24 characters' },
+    ],
+  },
+  {
+    name: 'readingA',
+    label: 'Reading A',
+    rules: [
+      {
+        required: true,
+        message: 'Please enter the Reading A!',
+      },
+    ],
+  },
+  {
+    name: 'readingB',
+    label: 'Reading B',
+    rules: [
+      {
+        required: true,
+        message: 'Please enter the Reading B!',
+      },
+    ],
+  },
+];
 
 const LogForm = (props) => {
-  const [componentSize, setComponentSize] = useState('small');
+  const history = useHistory();
+  const [log, setLog] = useState();
 
-  const onFormLayoutChange = ({ size }) => {
-    setComponentSize(size);
+  useEffect(() => {
+    const fetchData = async () => {
+      const log = await logService.getLogs('byId', props.logId);
+      setLog(log);
+    };
+
+    if (props.mode === 'edit') fetchData();
+  }, [props.logId, props.mode]);
+
+  const onConfirmSave = async (values) => {
+    if (props.mode === 'new') await onCreate(values);
+    if (props.mode === 'edit') await onUpdate(values);
+  };
+
+  const onCreate = async (values) => {
+    const response = await logService.createDevice(values);
+    if (response.status === 201) {
+      notification.success({
+        message: 'Entry Log created!!!',
+        description: 'The Entry Log has been successfully created.',
+      });
+      history.push(`/devices/${props.logId}`);
+    }
+  };
+
+  const onUpdate = async (values) => {
+    values.id = props.logId;
+    const response = await logService.updateLog(values);
+    if (response.status === 200) {
+      notification.success({
+        message: 'Entry Log modified!!!',
+        description: 'The Entry Log has been successfully modified.',
+      });
+      history.push(`/devices/${props.logId}`);
+    }
+  };
+
+  const onConfirmCancel = () => {
+    history.push(`/devices/${props.logId}`);
   };
 
   return (
-    <div>
-      <Form
-        labelCol={{
-          span: 4,
-        }}
-        wrapperCol={{
-          span: 14,
-        }}
-        layout="horizontal"
-        initialValues={{
-          size: componentSize,
-        }}
-        onValuesChange={onFormLayoutChange}
-        size={componentSize}
-      >
-        <Form.Item label="Form Size" name="size">
-          <Radio.Group>
-            <Radio.Button value="small">Small</Radio.Button>
-            <Radio.Button value="middle">Middle</Radio.Button>
-            <Radio.Button value="large">Large</Radio.Button>
-          </Radio.Group>
-        </Form.Item>
-        <Form.Item label="Device">
-          <Select>
-            <Select.Option value="demo">Demo</Select.Option>
-          </Select>
-        </Form.Item>
-        <Form.Item label="Date">
-          <DatePicker />
-        </Form.Item>
-        <Form.Item label="Reading">
-          <InputNumber />
-        </Form.Item>
-        <Form.Item>
-          <Button>Accept</Button>
-          <Button>Cancel</Button>
-        </Form.Item>
-      </Form>
-    </div>
+    <FormCreate
+      formItems={formItems}
+      onConfirmCancel={onConfirmCancel}
+      onConfirmSave={onConfirmSave}
+      entity='Log'
+      initialValues={log}
+    />
   );
 };
 
